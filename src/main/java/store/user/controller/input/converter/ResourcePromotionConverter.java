712293cac8da8d7dd691e8607aea9575ffd2b02e
Hyojae Kwon <70756219+hyojaeKwon@users.noreply.github.com>
2024-11-11 13:11:38 +0900

    refactor(user): dto제작에 사용되는 date를 LocalDateTime로 변경한다
    rule의 Date를 LocalDateTime으로 변경함에 따라 dto제작에 사용되는 date를 LocalDateTime로 변경한다

--- src/main/java/store/user/controller/input/converter/ResourcePromotionConverter.java
+++ src/main/java/store/user/controller/input/converter/ResourcePromotionConverter.java
@@ -1,11 +1,11 @@
 package store.user.controller.input.converter;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
+import store.item.controller.dto.request.PromotionRuleRequest;
 import store.user.controller.input.reader.InputFileReader;
 import store.user.controller.parser.InputParser;
-import store.item.controller.dto.request.PromotionRuleRequest;
 
 public class ResourcePromotionConverter {
     private static final int BUY_QUANTITY_INDEX = 1;
@@ -44,8 +44,8 @@ public class ResourcePromotionConverter {
         String name = rule.getFirst();
         long buyQuantity = parser.parseStringToLong(rule.get(BUY_QUANTITY_INDEX));
         long getQuantity = parser.parseStringToLong(rule.get(GET_QUANTITY_INDEX));
-        Date startDate = parser.parseStringToDate(rule.get(START_DAY_INDEX));
-        Date endDate = parser.parseStringToDate(rule.get(END_DAY_INDEX));
+        LocalDateTime startDate = parser.parseStringToDate(rule.get(START_DAY_INDEX));
+        LocalDateTime endDate = parser.parseStringToDate(rule.get(END_DAY_INDEX));
         return new PromotionRuleRequest(name, buyQuantity, getQuantity, startDate, endDate);
     }
 }
