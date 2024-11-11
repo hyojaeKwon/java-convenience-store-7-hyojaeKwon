56f544aef870e001c46643a150640e3f4de9b4a2
Hyojae Kwon <70756219+hyojaeKwon@users.noreply.github.com>
2024-11-11 16:32:42 +0900

    refactor(user): receipt formatter의 string을 상수로 분리한다
    receipt string formatter의 String들을 상수로 분리하여 유지보수성과 규칙성을 늘린다

--- src/main/java/store/user/controller/formatter/ReceiptOutputFormatter.java
+++ src/main/java/store/user/controller/formatter/ReceiptOutputFormatter.java
@@ -8,13 +8,25 @@ import store.purchase.domain.value.receipt.ReceiptPrice;
 public class ReceiptOutputFormatter {
 
     private static final String RECEIPT_HEADER = "==============W 편의점================";
-    private static final String ITEM_HEADER = String.format("%-19s%-10s%-6s", "상품명", "수량", "금액");
     private static final String PROMOTION_HEADER = "=============증     정===============";
+    private static final String ITEM_HEADER = String.format("%-19s%-10s%-6s", "상품명", "수량", "금액");
     private static final String PRICE_HEADER = "====================================";
+
     private static final String ITEM_FORMAT = "%-19s%-10d%-,6d";
     private static final String PROMOTION_FORMAT = "%-19s%-,16d";
     private static final String TOTAL_PRICE_FORMAT = "%-19s%-10d%-,6d";
     private static final String PRICE_FORMAT = "%-19s%-10s%s%-,5d";
+
+    private static final String NEW_LINE = "\n";
+    private static final String EMPTY_STRING = "";
+    private static final String BLANK_STRING = " ";
+    private static final String HYPHEN = "-";
+
+    private static final String TOTAL_PRICE_KOREAN = "총구매액";
+    private static final String PROMOTION_DISCOUNT_KOREAN = "행사할인";
+    private static final String MEMBERSHIP_DISCOUNT_KOREAN = "멤버십할인";
+    private static final String FINAL_PRICE_KOREAN = "내실돈";
+
 
     public String format(Receipt receipt) {
         StringBuilder sb = new StringBuilder();
@@ -25,34 +37,37 @@ public class ReceiptOutputFormatter {
     }
 
     private void formatPriceHeader(Receipt receipt, StringBuilder sb) {
-        sb.append(PRICE_HEADER).append("\n");
+        sb.append(PRICE_HEADER).append(NEW_LINE);
         formatPrice(receipt, sb);
         formatDiscountPrice(receipt.getPrice(), sb);
         formatTotalPrice(receipt.getPrice(), sb);
     }
 
     private void formatPrice(Receipt receipt, StringBuilder sb) {
-        String format = String.format(TOTAL_PRICE_FORMAT, "총구매액", receipt.totalPurchaseAmount(),
+        String format = String.format(TOTAL_PRICE_FORMAT, TOTAL_PRICE_KOREAN, receipt.totalPurchaseAmount(),
                 receipt.getPrice().getTotalPrice());
-        sb.append(format).append("\n");
+        sb.append(format).append(NEW_LINE);
     }
 
     private void formatDiscountPrice(ReceiptPrice price, StringBuilder sb) {
-        String promotionFormat = String.format(PRICE_FORMAT, "행사할인", "", "-", price.getPromotionDiscountPrice());
-        String membershipFormat = String.format(PRICE_FORMAT, "멤버십할인", "", "-", price.getMembershipDiscountPrice());
-        sb.append(promotionFormat).append("\n");
-        sb.append(membershipFormat).append("\n");
+        String promotionFormat = String.format(PRICE_FORMAT, PROMOTION_DISCOUNT_KOREAN, EMPTY_STRING, HYPHEN,
+                price.getPromotionDiscountPrice());
+        String membershipFormat = String.format(PRICE_FORMAT, MEMBERSHIP_DISCOUNT_KOREAN, EMPTY_STRING, HYPHEN,
+                price.getMembershipDiscountPrice());
+        sb.append(promotionFormat).append(NEW_LINE);
+        sb.append(membershipFormat).append(NEW_LINE);
     }
 
     private void formatTotalPrice(ReceiptPrice price, StringBuilder sb) {
-        String format = String.format(PRICE_FORMAT, "내실돈", "", " ",price.getResultPrice());
-        sb.append(format).append("\n");
+        String format = String.format(PRICE_FORMAT, FINAL_PRICE_KOREAN, EMPTY_STRING, BLANK_STRING,
+                price.getResultPrice());
+        sb.append(format).append(NEW_LINE);
     }
 
 
     private void formatReceiptHeader(Receipt receipt, StringBuilder sb) {
-        sb.append(RECEIPT_HEADER).append("\n");
-        sb.append(ITEM_HEADER).append("\n");
+        sb.append(RECEIPT_HEADER).append(NEW_LINE);
+        sb.append(ITEM_HEADER).append(NEW_LINE);
         for (PurchaseItem item : receipt.getPurchaseItems()) {
             formatPurchaseItem(item, sb);
         }
@@ -62,7 +77,7 @@ public class ReceiptOutputFormatter {
         if (receipt.getPromotionDiscountItems().isEmpty()) {
             return;
         }
-        sb.append(PROMOTION_HEADER).append("\n");
+        sb.append(PROMOTION_HEADER).append(NEW_LINE);
         for (PromotionDiscountItem item : receipt.getPromotionDiscountItems()) {
             formatPromotionItem(item, sb);
         }
@@ -70,12 +85,12 @@ public class ReceiptOutputFormatter {
 
     private void formatPromotionItem(PromotionDiscountItem item, StringBuilder sb) {
         String format = String.format(PROMOTION_FORMAT, item.getName(), item.getDiscountAmount());
-        sb.append(format).append("\n");
+        sb.append(format).append(NEW_LINE);
     }
 
     private void formatPurchaseItem(PurchaseItem item, StringBuilder sb) {
         String format = String.format(ITEM_FORMAT, item.getName(), item.getAmount(), item.getTotalPrice());
-        sb.append(format).append("\n");
+        sb.append(format).append(NEW_LINE);
     }
 }
 
