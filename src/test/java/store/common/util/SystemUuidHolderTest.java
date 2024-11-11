24d16fbe804fb51c9d83702fbbb9c486135c63e3
Hyojae Kwon <70756219+hyojaeKwon@users.noreply.github.com>
2024-11-11 21:58:46 +0900

    test(util): uuid holder의 test를 구현한다
    uuid holder가 regex를 이용하여 uuid를 생성하는지 확인하는 테스트를 구현한다.

---
+++ src/test/java/store/common/util/SystemUuidHolderTest.java
@@ -0,0 +1,27 @@
+package store.common.util;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.UUID;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class SystemUuidHolderTest {
+
+    private IdHolder idHolder;
+
+    @BeforeEach
+    void setUp() {
+        idHolder = new SystemUuidHolder();
+    }
+
+    @Test
+    @DisplayName("UUID를 생성하는지 확인한다.")
+    void generateUUIDtest() {
+        String id = idHolder.generateId();
+        Assertions.assertThat(id)
+                .matches("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$");
+    }
+}
